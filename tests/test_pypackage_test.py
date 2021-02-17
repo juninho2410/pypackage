832cf7e1d7937b50b3898c6179a62eae5754f551
José Mendes <juninho2410@hotmail.com>
2021-02-17 18:19:47 +0000

    adding fixture example in package

--- tests/test_pypackage_test.py
+++ tests/test_pypackage_test.py
@@ -9,19 +9,20 @@ from pypackage_test import pypackage_test
 
 
 @pytest.fixture
-def response():
+def read_file_ex(tmpdir):
     """Sample pytest fixture.
 
     See more at: http://doc.pytest.org/en/latest/fixture.html
     """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    file = tmpdir.join('test_read_file.txt')
+    with open(file,'w') as f: 
+        f.write("libs;version\n\tlib1;1\n")
+    yield file
 
-
-def test_read_file():
+def test_read_file(read_file_ex):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
-    test_df = pypackage_test.read_file('tests/test_read_file.txt')
+    test_df = pypackage_test.read_file(read_file_ex)
     test_columns= list(test_df.columns)
     assert 'libs' in test_columns
     assert test_df.loc[0][1]==1
